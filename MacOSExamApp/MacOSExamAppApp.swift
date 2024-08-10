95556418d4aac315f8dcb67669de669e09895973
Wout Van Cleemput <100853889+WoutVC@users.noreply.github.com>
2024-08-10 12:59:47 +0100

    Add files via upload

---
+++ MacOSExamApp/MacOSExamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSExamAppApp.swift
+//  MacOSExamApp
+//
+//  Created by user260450 on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MacOSExamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacOSExamAppUITests/MacOSExamAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MacOSExamAppUITestsLaunchTests.swift
+//  MacOSExamAppUITests
+//
+//  Created by user260450 on 10/08/2024.
+//
+
+import XCTest
+
+final class MacOSExamAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
